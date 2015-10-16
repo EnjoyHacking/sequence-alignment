ff25cd8493e9e94abd3ae6f1a86803c4c379bc20
syfry <sangyafei1026@gmail.com>
2015-10-16 16:51:38 +0800

    Init Commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#./main.py -w $1 -g -a http.raw 
+#./main.py -w $1 -p ~/PcapData/Pure/pure_dns.pcap 
+./main.py -h ~/workspace_for_github/ProDigger/related_files/http/http_merge_64_1000.hex 
